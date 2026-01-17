42de63a6d5d349a67569730e2f008e3e7bb9a8a7
jfeast <jordan.feast@outlook.com>
2026-01-17 20:40:12 +0000

    Added removeMeal command

--- command_removeMeal.go
+++ command_removeMeal.go
@@ -1,6 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func removeMeal(cfg *config, args ...string) error {
-	//TODO: implement removeMeal command
-	return nil
+	if len(args) < 1 {
+		return fmt.Errorf("Please provide a meal name")
+	}
+
+	mealName := strings.Join(args, " ")
+
+	for i, meal := range cfg.storedMeals {
+		if meal.Name == mealName {
+			cfg.storedMeals = append(cfg.storedMeals[:i], cfg.storedMeals[i+1:]...)
+			fmt.Printf("Meal '%s' removed successfully\n", mealName)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("Meal '%s' not found", mealName)
 }
